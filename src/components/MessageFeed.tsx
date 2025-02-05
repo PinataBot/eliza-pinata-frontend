7a8a1aec60b95fdcb16622ff2a1fb2a959d17173
igorlebedev <igorlebedev228@gmail.com>
2025-02-05 18:42:25 +0700

    add: scroll to bottom when renders

--- src/components/MessageFeed.tsx
+++ src/components/MessageFeed.tsx
@@ -1,11 +1,24 @@
+"use client";
 import { Message, MessageProps } from "@/components/Message";
+import { useEffect, useRef } from "react";
 
 export const MessageFeed = ({ messages }: { messages: MessageProps[] }) => {
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "instant" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <div className="w-full px-20 max-h-full auto overflow-scroll">
       {messages.map((message, index) => (
         <Message key={index} text={message.text} time={message.time} swapData={message.swapData} />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
