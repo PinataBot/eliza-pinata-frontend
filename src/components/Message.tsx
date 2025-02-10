4e5e6cc9a73552e33780aeb906ea6fe165350062
igorlebedev <igorlebedev228@gmail.com>
2025-02-10 17:10:52 +0700

    upd: rework message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,5 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Skeleton } from "@/components/ui/skeleton";
+"use client";
+import { SwapIcon } from "@/components/icons";
 
 export interface MessageProps {
   text: string;
@@ -18,28 +18,30 @@ export const Message = ({ text, time, swapData }: MessageProps) => {
   return (
     <div className="w-full my-4">
       {swapData && (
-        <div className="w-full flex gap-2 mb-1 items-center">
-          <h3 className="font-bold">SWAP:</h3>
-          <div className="flex rounded-2xl bg-green-300 justify-between gap-10 font-bold text-sm px-4 text-gray-700 w-full max-w-full">
-            <span>
-              {swapData.fromSwap} → {swapData.toSwap}
-            </span>
-            <span>
-              {swapData.amountFrom} → {swapData.amountTo}(<span>${swapData.usdAmount}</span>)
-            </span>
+        <div className="w-full -mb-3 pb-5 gap-2 rounded-t-xl px-5 pt-3 bg-gray-200 items-center">
+          <h3 className="font-bold text-3xl">Swap:</h3>
+          <div className="flex rounded-2xl justify-between gap-10 font-bold text-sms text-gray-700 w-full max-w-full">
+            <div className="flex gap-2 items-center">
+              <div className="rounded-full w-4 h-4 bg-red-300" />
+              <span>
+                {swapData.fromSwap} {swapData.amountFrom}
+              </span>
+            </div>
+            <SwapIcon />
+            <div className="flex gap-2 items-center">
+              <div className="rounded-full w-4 h-4 bg-red-300" />
+              <span>
+                {swapData.toSwap} {swapData.amountTo}
+              </span>
+            </div>
           </div>
         </div>
       )}
-      <div className="flex items-start gap-2">
-        <Avatar>
-          <AvatarImage src="https://t4.ftcdn.net/jpg/02/66/31/75/360_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg" />
-          <AvatarFallback>
-            <Skeleton className="h-12 w-12 rounded-full" />
-          </AvatarFallback>
-        </Avatar>
-        <div className="rounded-lg w-full text-white px-4 py-2 max-w-full justify-between bg-gray-950 flex items-center gap-2">
-          <span>{text}</span>
-          <span className="text-xs text-gray-400">{time}</span>
+      <div className="flex z-10 items-start gap-2">
+        <div className="rounded-xl border-[0.5px] border-gray-700 w-full text-gray-200 px-5 py-3 max-w-full bg-gray-950">
+          <h3 className="font-bold text-xl">Some heading maybe</h3>
+          <p className="font-medium line-clamp-4">{text}</p>
+          <p className="text-xs mt-2 text-gray-600">{time}</p>
         </div>
       </div>
     </div>
---
+++ src/components/icons/SwapIcon.tsx
@@ -0,0 +1,18 @@
+export const SwapIcon = ({ className }: { className?: string }) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path
+      d="M4 8H20M20 8L14.6667 4M20 8L14.6667 12M20 16H4M4 16L9.33333 12M4 16L9.33333 20"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
